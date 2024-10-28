471a5e3ffc61ad1f0aa092877495fc65ce5f6a08
unknown <nwanetifelix@gmail.com>
2024-10-28 21:07:07 +0100

    adding my recent projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,6 +4,7 @@ import Todo from "../../assets/todo.png";
 import Port from "../../assets/portfolio.png";
 import Foodapp from "../../assets/foodapp.png";
 import Maths from "../../assets/maths.png";
+import Campaign from "../../assets/campaign.png";
 
 const Portfolio = () => {
   return (
@@ -16,7 +17,12 @@ const Portfolio = () => {
             <img src={Port} alt="" />
           </div>
           <h3>My Portfolio Website</h3>
-
+          <div>
+            <small>
+              Used React, Mail integration to get direct message to my mail and
+              (Javascript Extension) JSX to make app interactive
+            </small>
+          </div>
           <div className="portfolio_item-cta">
             {" "}
             <a
@@ -24,7 +30,7 @@ const Portfolio = () => {
               className="btn"
               target="_blank"
             >
-              Gitub
+              Github
             </a>
             <a
               href="http://felixportfolio-nuf.vercel.app/"
@@ -37,9 +43,45 @@ const Portfolio = () => {
         </article>
         <article className="portfolio_item">
           <div className="portfolio_item-image">
+            <img src={Campaign} alt="" />
+          </div>
+          <h3>Campaign Website</h3>
+          <div>
+            <small>
+              Used React, Used axios get,post,put,delete method of the API to
+              get,create,edit and delete the arrays of the API and (Javascript
+              Extension) JSX to make app interactive
+            </small>
+          </div>
+          <div className="portfolio_item-cta">
+            {" "}
+            <a
+              href="https://github.com/NwanetiUche/campaign"
+              className="btn"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              href="https://campaign-nuf.vercel.app/"
+              className="btn btnPrimary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
             <img src={Foodapp} alt="" />
           </div>
           <h3>Food Recipe App</h3>
+          <div>
+            <small>
+              Used React, Made API call to get the food recipes and (Javascript
+              Extension) JSX to make app interactive
+            </small>
+          </div>
           <div className="portfolio_item-cta">
             {" "}
             <a
@@ -47,7 +89,7 @@ const Portfolio = () => {
               className="btn"
               target="_blank"
             >
-              Gitub
+              Github
             </a>
             <a
               href="https://foodapp-nuf.vercel.app/"
@@ -63,6 +105,11 @@ const Portfolio = () => {
             <img src={Todo} alt="" />
           </div>
           <h3>Todo App</h3>
+          <div>
+            <small>
+              Used React and (Javascript Extension) JSX to make app interactive
+            </small>
+          </div>
           <div className="portfolio_item-cta">
             {" "}
             <a
@@ -70,7 +117,7 @@ const Portfolio = () => {
               className="btn"
               target="_blank"
             >
-              Gitub
+              Github
             </a>
             <a
               href="https://todoapp-nuf.vercel.app/"
@@ -86,6 +133,9 @@ const Portfolio = () => {
             <img src={Maths} alt="" />
           </div>
           <h3>Maths Tutorial Website</h3>
+          <div>
+            <small>Used HTML and CSS with no framework</small>
+          </div>
           <div className="portfolio_item-cta">
             {" "}
             <a
@@ -93,7 +143,7 @@ const Portfolio = () => {
               className="btn"
               target="_blank"
             >
-              Gitub
+              Github
             </a>
             <a
               href="https://nwanetiuche.github.io/mathstutorial/GridMaths.html"
